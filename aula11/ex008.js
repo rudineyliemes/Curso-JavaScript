1b6820d695264df640439dc390152d8e5a681977
rudineyliemes <62783403+rudineyliemes@users.noreply.github.com>
2020-08-22 04:35:59 -0300

    desafio Nacionalidade
    Praticando condições JS.

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){ // Condição simples
+    console.log(`Você ultrapassou a velociade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
