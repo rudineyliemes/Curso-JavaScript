4c79f0870f70c094f00116e42d2581836e728bec
rudineyliemes <62783403+rudineyliemes@users.noreply.github.com>
2020-08-29 23:08:36 -0300

    Auka 12

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 22
+
+if (idade < 18) {
+    console.log(`Você tem ${idade} anos.`)
+} else if (idade < 18 || idade > 65){
+    console.log('Voto opcional')    
+} else {
+    console.log('Voto obrigatório')
+}
